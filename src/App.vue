c9f07d3e03414fbb678c77d26c47926a6936c0eb
puffluv <alibihasenov@gmail.com>
2025-01-09 01:49:00 +0700

    Optimize header and navigation - Take-out to individual components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { CheckCircleIcon, ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import TheHeader from './components/TheHeader.vue'
+import TheNav from './components/TheNav.vue'
 const navItems = {
   timeline: ClockIcon,
   activities: ListBulletIcon,
@@ -8,31 +10,9 @@ const navItems = {
 </script>
 
 <template>
-  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
-    <a href="#">
-      <img src="./assets/logo.png" alt="Logo" class="h-9" />
-    </a>
-    <a href="" class="text=sm">
-      <div v-if="false" class="flex items-center gap-1">
-        All tasks done!
-        <CheckCircleIcon class="h-7 text-green-900"></CheckCircleIcon>
-      </div>
-      <div v-else class="flex items-center gap-1">
-        <div>Progress: <span class="font-mono">20%</span></div>
-        <div class="h-3 w-3 rounded-full bg-red-900"></div>
-      </div>
-    </a>
-  </header>
+  <TheHeader />
 
   <main class="flex flex-grow flex-col"></main>
 
-  <nav class="sticky bottom-0 z-10 bg-white">
-    <ul class="flex items-center justify-around border-t">
-      <li v-for="(icon, page) in navItems" :key="page" class="flex-1">
-        <a :href="`#${page}`" class="flex flex-col items-center p2 text-xs capitalize">
-          <component :is="icon" class="h-6 w-6" /> {{ page }}
-        </a>
-      </li>
-    </ul>
-  </nav>
+  <TheNav />
 </template>
