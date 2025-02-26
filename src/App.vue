7d1f7b88292f9656221961a043077b8503db9a12
puffluv <alibihasenov@gmail.com>
2025-02-26 19:30:03 +0700

    Add generateTimelineItems

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,9 @@ import TheActivities from './pages/TheActivities.vue'
 import TheProgress from './pages/TheProgress.vue'
 import { ref } from 'vue'
 import { PAGE_ACTIVITIES, PAGE_TIMELINE, PAGE_PROGRESS } from './constants'
+import { normalizePageHash } from './functions'
 
 const currentPage = ref(normalizePageHash())
-
-function normalizePageHash() {
-  const hash = window.location.hash.slice(1)
-  if ([PAGE_ACTIVITIES, PAGE_TIMELINE, PAGE_PROGRESS].includes(hash)) {
-    return hash
-  }
-
-  window.location.hash = PAGE_TIMELINE
-  return PAGE_TIMELINE
-}
 
 function goTo(page) {
   currentPage.value = page
--- src/constants.js
+++ src/constants.js
@@ -8,3 +8,4 @@ export const NAV_ITEMS = {
   [PAGE_ACTIVITIES]: ListBulletIcon,
   [PAGE_PROGRESS]: ChartBarIcon,
 }
+export const HOURS_IN_DAY = 24
